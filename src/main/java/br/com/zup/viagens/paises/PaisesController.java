48f62655141577730442870241264b3632982ca2
Cássio Almeida <cassio.almeida@zup.com>
2021-03-03 16:22:39 -0300

    initial commit

---
+++ src/main/java/br/com/zup/viagens/paises/PaisesController.java
@@ -0,0 +1,11 @@
+package br.com.zup.viagens.paises;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/paises")
+public class PaisesController {
+
+
+}
