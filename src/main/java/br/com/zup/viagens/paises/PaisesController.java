38bd8c701fd2a86b71dbdc4ffbd6aea677615876
jaacksonalves <jacksonalves.pro@gmail.com>
2021-03-03 16:30:29 -0300

    Primeiro push rsrs

--- src/main/java/br/com/zup/viagens/paises/PaisesController.java
+++ src/main/java/br/com/zup/viagens/paises/PaisesController.java
@@ -1,11 +1,24 @@
 package br.com.zup.viagens.paises;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/paises")
 public class PaisesController {
 
+    @PersistenceContext
+    private EntityManager em;
 
+    @PostMapping
+    public String criaPais(@Valid @RequestBody NovoPaisRequest request){
+        return request.toString();
+
+    }
 }
