dfb8d290349e87d2536e4c7c195c3c66f2ec2def
jonas-souza-zup <jonas.souza@zup.com.br>
2021-03-10 16:40:40 -0300

    Endpoint para buscar pais

--- src/main/java/br/com/zup/viagens/paises/PaisesController.java
+++ src/main/java/br/com/zup/viagens/paises/PaisesController.java
@@ -1,19 +1,14 @@
 package br.com.zup.viagens.paises;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/api/paises")
@@ -31,5 +26,11 @@ public class PaisesController {
 
         URI uri = uriComponentsBuilder.path("/api/paises/{id}").buildAndExpand(pais.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/{id}")
+    @Transactional(readOnly = true)
+    public void listaPais(@PathVariable("id") Long id) {
+        manager.find(Pais.class, id);
     }
 }
